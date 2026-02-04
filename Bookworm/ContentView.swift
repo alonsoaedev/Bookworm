251b3c98fbccf268c7f49df527c59f95d5a7df8a
Alonso Acosta <alonso.ae@hotmail.com>
2026-02-04 16:49:22 -0600

    Create a custom component with @Binding

--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -7,15 +7,51 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct PushButton: View {
+    let title: String
+//    @State var isOn: Bool //uncomment to see the one way flow data effect
+    @Binding var isOn: Bool
+    
+    var onColors: [Color] = [.red, .yellow]
+    var offColors: [Color] = [.white.opacity(0.6), .white.opacity(0.4)]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Button(title) {
+            isOn.toggle()
         }
         .padding()
+        .background(
+            LinearGradient(
+                colors: isOn ? onColors : offColors,
+                startPoint: .top,
+                endPoint: .bottom
+            )
+        )
+        .foregroundStyle(.white)
+        .clipShape(.capsule)
+        .shadow(radius: isOn ? 0 : 5)
+    }
+}
+
+struct ContentView: View {
+    @State private var rememberMe = false
+    
+    var body: some View {
+        VStack {
+            // On this approach (one way flow of data)
+            // this won't change the state of remember because
+            // the rememberMe data will passed to PushButton and
+            // it will take control over the state on its own view and
+            // it won't pass the data back.
+            // This will create two sources of truth
+            // uncomment the line above to see the one way flow data effect
+            PushButton(
+                title: "Remember me",
+//                isOn: rememberMe // One way flow of data (two sources of truth)
+                isOn: $rememberMe // Two ways flow of data (onw source of truth)
+            )
+            Text(rememberMe ? "On" : "Off")
+        }
     }
 }
 
