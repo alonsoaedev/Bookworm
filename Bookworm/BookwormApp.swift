ba3f8d07d69de59eb5c0bc9c9ea8350b34934b42
Alonso Acosta <alonso.ae@hotmail.com>
2026-02-04 16:02:11 -0600

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Alonso Acosta on 04/02/26.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
