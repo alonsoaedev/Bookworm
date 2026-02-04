db153a28f3b45891644da60cf1323cc561ab9e48
Alonso Acosta <alonso.ae@hotmail.com>
2026-02-04 17:21:48 -0600

    Accept multi-line text input with TextEditor

--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -7,54 +7,47 @@
 
 import SwiftUI
 
-struct PushButton: View {
-    let title: String
-//    @State var isOn: Bool //uncomment to see the one way flow data effect
-    @Binding var isOn: Bool
-    
-    var onColors: [Color] = [.red, .yellow]
-    var offColors: [Color] = [.white.opacity(0.6), .white.opacity(0.4)]
-    
-    var body: some View {
-        Button(title) {
-            isOn.toggle()
-        }
-        .padding()
-        .background(
-            LinearGradient(
-                colors: isOn ? onColors : offColors,
-                startPoint: .top,
-                endPoint: .bottom
-            )
-        )
-        .foregroundStyle(.white)
-        .clipShape(.capsule)
-        .shadow(radius: isOn ? 0 : 5)
-    }
-}
-
 struct ContentView: View {
-    @State private var rememberMe = false
-    
+    @AppStorage("Bookworm__notes") private var notes = ""
     var body: some View {
-        VStack {
-            // On this approach (one way flow of data)
-            // this won't change the state of remember because
-            // the rememberMe data will passed to PushButton and
-            // it will take control over the state on its own view and
-            // it won't pass the data back.
-            // This will create two sources of truth
-            // uncomment the line above to see the one way flow data effect
-            PushButton(
-                title: "Remember me",
-//                isOn: rememberMe // One way flow of data (two sources of truth)
-                isOn: $rememberMe // Two ways flow of data (onw source of truth)
-            )
-            Text(rememberMe ? "On" : "Off")
+        NavigationStack {
+            List {
+                NavigationLink("TextField y TextEditor (no form)") {
+                    Text("Text Editor")
+                    TextEditor(text: $notes)
+                        .textFieldStyle(.roundedBorder)
+                    
+                    Text("Text Field (horizontal)")
+                    TextField("Enter your text", text: $notes)
+                        .textFieldStyle(.roundedBorder)
+                    
+                    Text("Text Field (vertical)")
+                    TextField("Enter your text", text: $notes, axis: .vertical)
+                        .textFieldStyle(.roundedBorder)
+                }
+                
+                NavigationLink("TextField y TextEditor (form)") {
+                    Form {
+                        Text("Text Editor")
+                        TextEditor(text: $notes)
+                            .textFieldStyle(.roundedBorder)
+                        
+                        Text("Text Field (horizontal)")
+                        TextField("Enter your text", text: $notes)
+                            .textFieldStyle(.roundedBorder)
+                        
+                        Text("Text Field (vertical)")
+                        TextField("Enter your text", text: $notes, axis: .vertical)
+                            .textFieldStyle(.roundedBorder)
+                    }
+                }
+            }
+            .navigationTitle("")
         }
     }
 }
 
 #Preview {
     ContentView()
+        .preferredColorScheme(.dark)
 }
