2800b983655320447786baa162ff4b4cdb8f471f
Alonso Acosta <alonso.ae@hotmail.com>
2026-02-05 12:25:48 -0600

    Build a list with @Query

---
+++ Bookworm/EmojiRatingView.swift
@@ -0,0 +1,26 @@
+//
+//  EmojiRatingView.swift
+//  Bookworm
+//
+//  Created by Alonso Acosta on 05/02/26.
+//
+
+import SwiftUI
+
+struct EmojiRatingView: View {
+    let rating: Int
+    
+    var body: some View {
+        switch rating {
+        case 1: Text("☹️")
+        case 2: Text("🥱")
+        case 3: Text("🫤")
+        case 4: Text("😊")
+        default: Text("😱")
+        }
+    }
+}
+
+#Preview {
+    EmojiRatingView(rating: 3)
+}
